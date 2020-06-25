eefba93a16f08663f2ba954e8d74f0ab4d87695f
Niyi Enny <myjessy.sr@gmail.com>
2020-06-25 15:31:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+
+/**
+ * This is an example of a front controller for a flat file PHP site. Using a
+ * Static list provides security against URL injection by default. See README.md
+ * for more examples.
+ */
+# [START gae_simple_front_controller]
+switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
+    case '/':
+        require 'index.php';
+        break;
+    case '/portal.php':
+        require 'portal.php';
+        break;
+    default:
+        http_response_code(404);
+        exit('Not Found');
+}
+# [END gae_simple_front_controller]
+
+?>
+
 <!doctype html>
 <html>
 <head>
